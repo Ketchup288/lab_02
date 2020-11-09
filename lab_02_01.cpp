ad5b82c2959920cea08f98f06660b2cd1d02f39b
Ketchup288 <arsenko525@gmail.com>
2020-11-09 22:13:11 +0200

    hello

--- lab_02_01.cpp
+++ lab_02_01.cpp
@@ -12,7 +12,7 @@ using namespace std;
 int main(){
     double a, z2, z1; // a - змінна для обчислення, z2 i z1 - змінні, які далі будуть обозначати рішення обох прикладів.
     cout << "Input your number here: "; cin >> a;
-    z1 = (1 - 2 * pow(sin(a), 2))/(1 + sin(2) * a); // рішення першого прикладу
+    // z1 = (1 - 2 * pow(sin(a), 2))/(1 + sin(2) * a); // рішення першого прикладу
     cout << "Your answer for the first function is: " << z1 << endl;
     z2 = (1 - tan(a))/(1+tan(a)); // рішення другого прикладу
     cout << "Your answer for the second function is: " << z2 << endl;
